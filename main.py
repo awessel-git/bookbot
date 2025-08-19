f6b4d0bc28c7b0464d80eb8b4c2ddbea6705f597
antonwessel <a.wessel.dev@gmail.com>
2025-08-19 20:52:30 +0200

    Refactor main.py to improve error handling and enhance user feedback for file path issues; tidy up stats.py with consistent formatting

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 from stats import count_words, count_characters, sort_characters
 import sys
 
+
 def get_book_text(filepath):
     with open(filepath) as f:
         book_text = f.read()
         return book_text
-    
+
+
 def main():
     if len(sys.argv) < 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
-    
     filepath = sys.argv[1]
-    frankenstein_text = get_book_text(filepath)
-    words_count = count_words(frankenstein_text)
-    characters_count = count_characters(frankenstein_text)
-    sorted_characters = sort_characters(characters_count)
 
-    print_report(filepath, words_count, sorted_characters)
+    try:
+        book_text = get_book_text(filepath)
+        words_count = count_words(book_text)
+        characters_count = count_characters(book_text)
+        sorted_characters = sort_characters(characters_count)
+        print_report(filepath, words_count, sorted_characters)
+    except FileNotFoundError:
+        print("Couldn't find the book. Try again with the correct filepath and remember the file extension, e.g., 'python3 main.py books/crime_and_punishment.txt'")
+
 
 def print_report(filepath, words_count, sorted_characters):
     print("------ BOOKBOT ------")
@@ -28,5 +33,6 @@ def print_report(filepath, words_count, sorted_characters):
     for item in sorted_characters:
         print(f"{item["char"]}: {item["count"]}")
     print("^^^^^^ END ^^^^^^")
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,7 @@
 def count_words(book_text):
     return len(book_text.split())
 
+
 def count_characters(book_text):
     characters = {}
     for letter in book_text:
@@ -11,7 +12,9 @@ def count_characters(book_text):
             characters[lowercase_letter] = 1
     return characters
 
+
 def sort_characters(characters):
+    # sorts the characters from highest to lowest count
     sorted_characters = []
     for k, v in characters.items():
         if k.isalpha():
@@ -21,6 +24,7 @@ def sort_characters(characters):
             sorted_characters.append(new_entry)
     sorted_characters.sort(reverse=True, key=sort_on)
     return sorted_characters
+
 
 def sort_on(items):
     return items["count"]
